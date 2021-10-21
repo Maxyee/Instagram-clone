3ee59f44563f07c14ea5ca9759580de561c5d88f
maxyee <eyaminkhan00@gmail.com>
2021-10-21 17:56:34 +0100

    building the right section of Header

--- components/Header.js
+++ components/Header.js
@@ -12,7 +12,7 @@ import { HomeIcon } from "@heroicons/react/solid";
 
 export default function Header() {
   return (
-    <div className="flex justify-between bg-white max-w-6xl">
+    <div className="flex justify-between bg-white max-w-6xl mx-5 lg:mx-auto">
       {/* Left */}
       <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
         <Image
@@ -44,6 +44,26 @@ export default function Header() {
       </div>
 
       {/* Right */}
+      <div className="flex items-center justify-end space-x-4">
+        <HomeIcon className="navBtn" />
+        <MenuIcon className="h-6 md:hidden cursor-pointer" />
+
+        <div className="relative navBtn">
+          <PaperAirplaneIcon className="navBtn rotate-45" />
+          <div className="absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">
+            3
+          </div>
+        </div>
+        <PlusCircleIcon className="navBtn" />
+        <UserGroupIcon className="navBtn" />
+        <HeartIcon className="navBtn" />
+
+        <img
+          className="h-10 rounded-full cursor-pointer"
+          src="https://links.papareact.com/3ke"
+          alt="profile pic"
+        />
+      </div>
     </div>
   );
 }
